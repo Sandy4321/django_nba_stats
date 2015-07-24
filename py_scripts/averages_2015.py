4c8ca6013b1eae42249c4e2c1e193bfbd3472393
fredthekid <fredthekid@gmail.com>
2015-07-24 14:57:41 -0700

    Update averages_2015.py

--- py_scripts/averages_2015.py
+++ py_scripts/averages_2015.py
@@ -1,15 +1,14 @@
 ##Averages Table Schema:
 import MySQLdb
-
 def calc_and_store_averages(season):
     db = MySQLdb.connect(host="localhost",  user="fred",  db="nba_analysis")
     cur = db.cursor()
     id_list = []
 
     season_string = '2' + str(season-1)
-    cur.execute("SELECT * FROM id_player WHERE to_year=" + str(season) + " AND from_year<" + str(season) + ";")
-    for row in cur:
-        id_list.append(int(row[1]))
+    # cur.execute("SELECT * FROM id_player WHERE to_year=" + str(season) + " AND from_year<" + str(season) + ";")
+    # for row in cur:
+    #     id_list.append(int(row[1]))
 
     for players in range(0, len(id_list)):
         insert_string = "INSERT INTO avg_2015 VALUES(" + str(id_list[players]) + ", "
@@ -28,22 +27,42 @@ def calc_and_store_averages(season):
         cur.execute("SELECT COUNT(*), AVG(min), AVG(fgm), AVG(fga), SUM(fgm)/SUM(fga), "
                     "AVG(fg3m), AVG(fg3a), SUM(fg3m)/SUM(fg3a), AVG(ftm), AVG(fta), SUM(ftm)/SUM(fta), "
                     "AVG(oreb), AVG(dreb), AVG(reb), AVG(ast), AVG(stl), AVG(blk),"
-                    "AVG(tov), AVG(foul), AVG(pts), AVG(plus_minus) "
+                    "AVG(tov), AVG(ast)/AVG(tov), AVG(foul), AVG(pts), AVG(plus_minus) "
                     "FROM game_log_2015 "
                     "WHERE season_id=" + season_string + " AND player_id=" + str(id_list[players]) + ";")
         db_read = cur.fetchone()
 
         for i in range(0, len(db_read)):
             if i != len(db_read) - 1:
-                insert_string += str(db_read[i]) + ", "
+                if str(db_read[i]) == 'None':
+                    insert_string += '0.0000' + ", "
+                else:
+                    insert_string += str(db_read[i]) + ", "
             else:
-                insert_string += str(db_read[i]) + ")"
+                if str(db_read[i]) == 'None':
+                    insert_string += '0.0000' + ")"
+                else:
+                    insert_string += str(db_read[i]) + ")"
 
         print(insert_string)
+        cur.execute(insert_string)
+        db.commit()
+
+def atr_update_i_goofed(season):
+    db = MySQLdb.connect(host="localhost",  user="fred",  db="nba_analysis")
+    cur = db.cursor()
+    id_list = []
+
+    cur.execute("SELECT * FROM id_player WHERE to_year=" + str(season) + " AND from_year<" + str(season) + ";")
+    for row in cur:
+        id_list.append(int(row[0]))
+
+    for player in range(0, len(id_list)):
+        cur.execute("SELECT ast, tov FROM avg_2015 WHERE player_id=" + str(id_list[player]) + ";")
+        stats = cur.fetchone()
         try:
-            cur.execute(insert_string)
+            cur.execute("UPDATE avg_2015 SET atr=" + str(float(stats[0])/float(stats[1])) + "WHERE player_id=" + str(id_list[player]) + ";")
             db.commit()
         except:
-            print("Some Shit Happend")
-
-calc_and_store_averages(2015)
+            print(id_list[player])
+            print("FUCK, NOT IN HERE")
